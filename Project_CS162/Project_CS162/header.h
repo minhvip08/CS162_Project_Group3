23babeea336a57b714ccef1d91ca7952b26bfc2d
tttrinhtran <89054365+tttrinhtran@users.noreply.github.com>
2022-03-15 16:39:58 +0700

    demo cont

--- Project_CS162/Project_CS162/header.h
+++ Project_CS162/Project_CS162/header.h
@@ -38,18 +38,37 @@ struct Date{
 struct Courses{ 
 	int ID_course; 
 	string course_name; 
-	string teacher_
+	string teacher_name; 
+	unsigned int credits; 
+	//int max_student = MAX; 
+	int cur_student; 
+	session_date session_date; 
+	session_time session_time;
+	int num_sessions; 
 };
 
 struct schoolYear{ 
-	
+	string time; 
+	int courses; 
 };
 
-struct Teacher{ 
-	int 
+struct Teacher{
 };
-int main() {
-	cout << "hello";
-}
+
+struct session_date{ 
+	const string MON = "MON"; 
+	const string TUE = "TUE"; 
+	const string WED = "WED"; 
+	const string THU = "THU"; 
+	const string FRI = "FRI"; 
+	const string SAT = "SAT"; 
+};
+
+struct session_time{
+	const string S1 = "S1(7:30)"; 
+	const string S2 = "S2(09:30"; 
+	const string S3 = "S3(13:30)";
+	const string S4 = "S4(15:30)"; 
+};
 
 
