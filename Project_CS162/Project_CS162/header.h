55adf289f83dec31096b3d1d667e62a363c4ce3c
tttrinhtran <89054365+tttrinhtran@users.noreply.github.com>
2022-03-15 15:55:52 +0700

    File header demo

--- Project_CS162/Project_CS162/header.h
+++ Project_CS162/Project_CS162/header.h
@@ -1,3 +1,35 @@
 #pragma once
 #include <iostream>
-using namespace std;
+#include <fstream>
+
+struct Student{ 
+	int ID_student; 
+	string FirstName; 
+	string LastName; 
+	char Gender; 
+	string birthday; 
+	string Social_ID; 
+};
+
+struct Date{ 
+	int day, month, year; 
+};
+
+struct Courses{ 
+	int ID_course; 
+	string course_name; 
+	string teacher_
+};
+
+struct schoolYear{ 
+	
+};
+
+struct Teacher{ 
+	int 
+};
+int main() {
+	cout << "hello";
+}
+
+
