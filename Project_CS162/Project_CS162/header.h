5e37c5f52ba527324ea04f707e262987ad0c4a31
minhvip08 <minhvip08@gmail.com>
2022-03-22 09:10:34 +0700

    update Header

--- Project_CS162/Project_CS162/header.h
+++ Project_CS162/Project_CS162/header.h
@@ -9,25 +9,25 @@ using namespace std;
 
 #define MAX 50
 
-/*schoolyear: add head; 
+/*schoolyear: add head;
 semester: add head; // use recursion to output
 student list: add head; // use recursion to output
 course: add head;
-class: add head; */ 
+class: add head; */
 
 struct date {
 	int day, month, year;
 };
 
-struct profile{
-	string lastname; 
+struct profile {
+	string lastname;
 	string firstname;
-	date DOB; 
+	date DOB;
 	string gender;
-	string social_id; 
+	string social_id;
 };
 
-struct account{
+struct account {
 	string username;
 	string password;
 	int type; // 0 = student, 1 = staff.
@@ -51,71 +51,73 @@ struct session_time {
 	const string S4 = "S4(15:30)";
 };
 
-struct session{
-	string date; 
-	string time;  
+struct session {
+	string date;
+	string time;
 };
 
 struct Class {
 	string class_name;
 	int total_student;  // input <= 50 (opt 1. input, 2.default)
-	date start_date; 
-	date end_day; 
-	student* student_list; 
-	Class* nextClass; 
+	date start_date;
+	date end_day;
+	student* student_list;
+	Class* nextClass;
 };
 
-struct course{ // thieu student list
-	int ID_course; 
-	string course_name; 
+struct course { // thieu student list
+	int ID_course;
+	string course_name;
 	string teacher_name;
-	unsigned int credits;  
-	int cur_student; 
-	session *sess;
-	student* student_list; 
+	unsigned int credits;
+	int cur_student;
+	session* sess;
+	student* student_list;
 	//studentScore* list_score; 
-	course* next; 
+	course* next;
 	//int num_sessions; 
 };
 
-struct semester { 
-	date start_day;
-	date end_day;
+struct semester {
+	date start_date;
+	date end_date;
+	date start_regist;
+	date end_regist;
 	int num_course = 0;
-	int num_class = 0; 
+	int num_class = 0;
 	course* course_list;
-//	Class* list_class; 
-	semester* next; 
+	//	Class* list_class; 
+	semester* next;
 };
 
 struct schoolyear {
 	string time;
-	int num_sem = 0;
+	int num_sem = 3;
 	semester* sem;
-	Class* list_class; 
+	Class* list_class;
 	schoolyear* next_schyear;
 };
 
-struct student 
+struct student
 {
-	int No; 
-	string id; 
+	int No;
+	string id;
 	profile prf;
 	double gpa;
-	account acc; 
-//	course* listCourse;
-	enrolledCourse* list_enrolled; 
-	finalGPA* list_gpa; 
+	account acc;
+	//	course* listCourse;
+	enrolledCourse* list_enrolled;
+	finalGPA* list_gpa;
 	student* pNext;
 };
 
 struct enrolledCourse {
-	int id_course; 
-	string name; 
-	session *sess;
-	studentScore* list_score; 
-	enrolledCourse* next; 
-	/*int semester; 
+	int id_course;
+	string name;
+	session* sess;
+	studentScore* list_score;
+	enrolledCourse* next;
+	/*int semester;
 	int year;*/
 };
 
@@ -130,8 +132,8 @@ struct studentScore {
 };
 
 struct finalGPA {
-	float gpa; 
-	finalGPA* next; 
+	float gpa;
+	finalGPA* next;
 };
 
 
@@ -157,3 +159,7 @@ void Delete_semester(semester*& head);
 void Delete_course(course*& head);
 void Delete_session(session*& head);
 void Delete_class(Class*& head);
+
+// Course
+void createSemester(schoolyear* pHead);
+void createCourseEnrolledTime(semester*& pTemp);
