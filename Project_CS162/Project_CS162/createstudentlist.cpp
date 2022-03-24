9c5baab98402c83fb5b0d92b2d3072781b1a10a2
ngminhkha1 <minhkha2303sv@gmail.com>
2022-03-24 21:21:40 +0700

    Fix bugs and Export
    the old code now can work with the new header and add a function that export students of a course to the csv file

--- Project_CS162/Project_CS162/createstudentlist.cpp
+++ Project_CS162/Project_CS162/createstudentlist.cpp
@@ -1,87 +1,114 @@
-//#include "header.h"
-//student* inputonestu(student** cur)
-//{
-//
-//}
-//void createlistofstu(Class*& head, const string str) //str is the id of the class
-//{
-//	int no = 0;
-//	ifstream fin;
-//	fin.open(str + ".csv");
-//	if (!fin.eof())
-//	{
-//		cout << "Can't open the file!\n";
-//		return;
-//	}
-//	student* cur = head->stu;
-//	while (!fin.eof())
-//	{
-//		++no;
-//		if (head -> stu == nullptr)
-//		{
-//			head->stu = new student;
-//			head->stu->prf.no = no;
-//			getline(fin, head->stu->prf.id, ',');
-//			getline(fin, head->stu->prf.firstname, ',');
-//			getline(fin, head->stu->prf.lastname, ',');
-//			getline(fin, head->stu->prf.gender, ',');
-//			getline(fin, head->stu->prf.birthday, ',');
-//			getline(fin, head->stu->prf.social_id, ',');
-//			head->stu->pNext = nullptr;
-//			cur = head->stu;
-//			cur->pNext = nullptr;
-//		}
-//		else
-//		{
-//			cur->pNext = new student;
-//			cur = cur->pNext;
-//			cur->prf.no = no;
-//			getline(fin, cur->prf.id, ',');
-//			getline(fin, cur->prf.firstname, ',');
-//			getline(fin, cur->prf.lastname, ',');
-//			getline(fin, cur->prf.gender, ',');
-//			getline(fin, cur->prf.birthday, ',');
-//			getline(fin, cur->prf.social_id, ',');
-//			cur->pNext = nullptr;
-//		}
-//	}
-//	fin.close();
-//}
-//void copystudentinfo(student* stu1, student* stu2)
-//{
-//	if (stu1 == nullptr)
-//		return;
-//	stu1->prf.no = stu2->prf.no;
-//	stu1->prf.firstname =stu2->prf.firstname;
-//	stu1->prf.lastname, stu2->prf.lastname;
-//	stu1->prf.id, stu2->prf.id;
-//	stu1->prf.gender, stu2->prf.gender;
-//	stu1->prf.social_id, stu2->prf.social_id;
-//	stu1->prf.birthday, stu2->prf.birthday;
-//
-//}
-//void copyliststu(Class*& c1, Class* c2) // c1 is the new class (dont have any students)
-//{
-//	
-//	if (c2->stu == nullptr)
-//		return;
-//	student* tmp = nullptr;
-//	student* cur = c2->stu;
-//	while (cur)
-//	{
-//		if (c1->stu == nullptr)
-//		{
-//			copystudentinfo(c1->stu, cur);
-//			tmp = c1->stu;
-//		}
-//		else
-//		{
-//			tmp->pNext = new student;
-//			tmp = tmp->pNext;
-//			copystudentinfo(tmp, cur);
-//			tmp->pNext = nullptr;
-//		}
-//		cur = cur->pNext;
-//	}
-//
-//}
+#include "header.h"
+
+void createlistofstu(Class*& head, const string str) //str is the id of the class
+{
+	int no = 0;
+	ifstream fin;
+	fin.open(str + ".csv");
+	if (!fin.eof())
+	{
+		cout << "Can't open the file!\n";
+		return;
+	}
+	student* cur = head->student_list;
+	while (!fin.eof())
+	{
+		++no;
+		if (head -> student_list == nullptr)
+		{
+			head->student_list = new student;
+			head->student_list->No = no;
+			getline(fin, head->student_list->id, ',');
+			getline(fin, head->student_list->prf.firstname, ',');
+			getline(fin, head->student_list->prf.lastname, ',');
+			getline(fin, head->student_list->prf.DOB, '/');
+			getline(fin, head->student_list->prf.gender, ',');
+			getline(fin, head->student_list->prf.social_id, ',');
+			head->student_list->pNext = nullptr;
+			cur = head->student_list;
+			cur->pNext = nullptr;
+		}
+		else
+		{
+			cur->pNext = new student;
+			cur = cur->pNext;
+			cur->No = no;
+			getline(fin, cur->id, ',');
+			getline(fin, cur->prf.firstname, ',');
+			getline(fin, cur->prf.lastname, ',');
+			getline(fin, cur->prf.gender, ',');
+			getline(fin, cur->prf.DOB, ',');
+			getline(fin, cur->prf.social_id, ',');
+			cur->pNext = nullptr;
+		}
+	}
+	fin.close();
+}
+void copystudentinfor(student* &stu1, student* &stu2)
+{
+	if (stu1 == nullptr)
+		return;
+	stu1->No = stu2->No;
+	stu1->prf.firstname = stu2->prf.firstname;
+	stu1->prf.lastname = stu2->prf.lastname;
+	stu1->id = stu2->id;
+	stu1->prf.gender = stu2->prf.gender;
+	stu1->prf.social_id = stu2->prf.social_id;
+	stu1->prf.DOB = stu2->prf.DOB;
+
+}
+void copyliststu(Class*& c1, Class* c2) // c1 is the new class (dont have any students)
+{
+	
+	if (c2->student_list == nullptr)
+		return;
+	student* tmp = c1->student_list;
+	student* cur = c2->student_list;
+	while (cur)
+	{
+		if (c1->student_list == nullptr)
+		{
+			copystudentinfor(c1->student_list, cur);
+			tmp = c1->student_list;
+		}
+		else
+		{
+			tmp->pNext = new student;
+			tmp = tmp->pNext;
+			copystudentinfor(tmp, cur);
+			tmp->pNext = nullptr;
+		}
+		cur = cur->pNext;
+	}
+
+}
+void exportonestu(ofstream& fout, student* stu)
+{
+	fout << stu->No << ',';
+	fout << stu->id << ',';
+	fout << stu->prf.firstname << ',';
+	fout << stu->prf.lastname << ',';
+	fout << stu->prf.gender << ',';
+	fout << stu->prf.DOB << ',';
+	fout << stu->prf.social_id << ',' << endl;
+}
+void exportlistofstudent(Class* c, course *cour)
+{
+	ofstream fout;
+	fout.open(cour->course_name + ".csv");
+	studentScore* cur = cour->list_score;
+	while (cur)
+	{
+		student* stu = c->student_list;
+		while (stu)
+		{
+			if (stu->id == cur->id)
+			{
+				exportonestu(fout, stu);
+			}
+			stu = stu->pNext;
+		}
+		cur = cur->pNext;
+	}
+	fout.close();
+}
