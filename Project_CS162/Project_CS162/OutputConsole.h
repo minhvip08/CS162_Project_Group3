71c2570c06d6bd638c378f78bba706c0e7ec9e32
tttrinhtran <trantran0603@gmail.com>
2022-04-22 10:59:34 +0700

    bổ sung file thiếu :DD

---
+++ Project_CS162/Project_CS162/OutputConsole.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Header.h"
+void show_semester(schoolyear*& head, semester*& s);
+void show_course(course* c, int i);
+void show_ID_course(course* c);
+void show_enrolled_course(enrolledCourse* c);
+void show_session(session s);
+void show_class(Class* head, int i);
+void show_date(date s);
+void show_schoolyear(schoolyear* head);
+void showListEnrolledCourse(student* pS); // chua viet
+void viewscoreboard(course* c);
+void showStudentsInClass(Class* pC);
+void showStudentsInCourse(course* pC);
